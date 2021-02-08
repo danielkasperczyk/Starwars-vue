ffa8e52c2d3a070e60af7556c2b6d0a30021f9b0
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2021-02-08 09:15:17 +0100

    fetching,getting and setting heroes

--- src/store/modules/heroes.js
+++ src/store/modules/heroes.js
@@ -1,22 +1,30 @@
 import axios from 'axios';
 
-const heroes = {
-    state: () => ({
-        heroes:     []
-    }),
-    
-    mutations: () => ({
+export default {
+    state:{
+        heroes: []
+    },
+    getters: {
+        returnHero(state){
+            if(state.heroes.length > 0) {
+                return state.heroes;
+            }
+        }
+    },
+    mutations: {
         setHeroes(state, payload){
             state.heroes = payload;
         }
-    }),
+    },
     
-    actions: () => ({
-        async fetchHeroes({ comit },state, payload){
-            const data = await axios.get(`https://swapi.dev/api/people/?search=${payload}`)
-            comit.setHeroes(data);
+    actions: {
+        async fetchHeroes(context, text){
+            const response = await axios.get(`https://swapi.dev/api/people/?search=${text}`)
+            const data = await response.data.results;
+            const endData = data.length > 0 ? data : "Hero not found";
+            context.commit("setHeroes", endData);
         }
-    })
+    }
 }
 
-export default heroes;
+

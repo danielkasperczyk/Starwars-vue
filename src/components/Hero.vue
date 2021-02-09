d8b2333744ff7df15e6d6a0aae4d13efd9e94061
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2021-02-09 12:31:06 +0100

    display information about picked hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container fluid>
+        <v-card
+            max-width="400"
+            class="mx-auto text">
+            <v-list-item v-for="(value, index) in this.hero" :key="index">
+                <v-list-content>
+                    <v-list-item-title>{{value}}</v-list-item-title>
+                    <v-list-item-subtitle>{{index.replace('_', ' ')}}</v-list-item-subtitle>
+                </v-list-content>
+            </v-list-item>
+    </v-card>
+    <v-btn
+      class="ma-2"
+      outlined
+      to="/"
+    >
+      Back to Home
+    </v-btn>
+    </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'Hero',
+    data(){
+        return{
+            hero: {}
+        }
+    },
+    props: {
+        heroData: {
+            type: Object,
+            required: true
+        }
+    },
+     async created(){
+        const { name, mass, height, hair_color, skin_color, homeworld } = this.heroData;
+        const homeRes = await axios.get(`${homeworld}`);
+        const world = await homeRes.data.name;
+        this.hero = {
+            name, 
+            mass, 
+            height, 
+            hair_color, 
+            skin_color,
+            world
+        }
+        console.log(this.hero);
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,19 +1,25 @@
 <template>
     <v-container>
-        <v-list v-if="Array.isArray(findedHeroes) ">
+        <v-list 
+            v-if="Array.isArray(findedHeroes)"
+            max-width="600"
+            class="ma-auto">
             <v-list-item-group>
                 <v-list-item
-                    v-for="(hero,index ) in findedHeroes"
-                    :key="index">
+                    v-for="(hero,index) in findedHeroes"
+                    :key="index"
+                    :to="{name: 'hero', params:{heroId: hero.name, heroData: hero}}">
                         <v-list-item-content>
-                            <v-list-item-title 
-                                v-text="hero.name"
-                                class="text-center"/>
+                           
+                                <v-list-item-title 
+                                    v-text="hero.name"
+                                    class="text-center"/>
+                                
                         </v-list-item-content>
                 </v-list-item>
             </v-list-item-group>
         </v-list>
-        <h3 v-else class="text-center" v-text="findedHeroes"></h3>
+        <p v-else class="text-center" v-text="findedHeroes"></p>
     </v-container>
 </template>
 
@@ -23,7 +29,7 @@ export default {
 
     computed: {
         findedHeroes(){
-            return this.$store.getters.returnHero;
+            return this.$store.getters.returnHero
         }
     }
 }

1a26f6252c42af0437040b1c9aa9af1336f4ab63
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2021-02-16 17:49:50 +0100

    displaying text if hero is not found

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,34 +1,35 @@
 <template>
-    <v-container class="d-flex flex-column">
+    <v-container 
+        class="d-flex flex-column pa-0">
         <v-select
+            v-model="selected"
             v-show="Array.isArray(findedHeroes)"
             :items=filter
-            :value=filter[0]
-            class="align-self-end"
-            @change="filterHeroes">
+            class="align-self-center">
         </v-select>
         <v-list 
-            v-if="this.heroes"
+            v-if="Array.isArray(findedHeroes)"
             max-width="600"
             width="100%"
             class="ma-auto">
             <v-list-item-group>
                 <v-list-item
-                    v-for="(hero,index) in this.heroes"
+                    v-for="(hero,index) in filterHeroes"
                     :key="index"
                     :gender="hero.gender"
                     :to="{name: 'hero', params:{heroId: hero.name, heroData: hero}}">
                         <v-list-item-content>
-                           
                                 <v-list-item-title 
                                     v-text="hero.name"
                                     class="text-center"/>
-                                
                         </v-list-item-content>
                 </v-list-item>
             </v-list-item-group>
         </v-list>
-        <p v-else class="text-center" v-text="findedHeroes"></p>
+        <p 
+            v-else 
+            class="text-center" 
+            v-text="findedHeroes"></p>
     </v-container>
 </template>
 
@@ -38,27 +39,17 @@ export default {
     data(){
         return{
             filter: ['All', 'Male', 'Female', 'Unknown'],
-            heroes: []
+            selected: 'All'
         }
     },
     computed: {
         findedHeroes(){
             return this.$store.getters.returnHero
+        },
+        filterHeroes(){
+            return this.$store.getters.filteredHeroes(this.selected)
         }
     },
-    methods: {
-        filterHeroes(gender){
-            const heroes = this.$store.getters.returnHero;
-            if(gender === "All"){
-                this.heroes = heroes;
-                console.log(this.heroes);
-            }
-            else{
-                this.heroes = heroes.filter(hero => hero.gender.toUpperCase() === gender.toUpperCase());
-                console.log(this.heroes);
-            }
-        }
-    }
 }
 </script>
 

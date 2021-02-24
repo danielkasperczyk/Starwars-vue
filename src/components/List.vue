6cbd630e98708e6ee2e4e4db244bbf2390f47a10
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2021-02-24 10:30:55 +0100

    displaying v-select on center of the screen

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,58 +1,41 @@
 <template>
-    <v-container 
-        class="d-flex flex-column pa-0">
-        <v-select
-            v-model="selected"
-            v-show="Array.isArray(findedHeroes)"
-            :items=filter
-            class="align-self-center">
-        </v-select>
-        <v-list 
-            v-if="Array.isArray(findedHeroes)"
-            max-width="600"
-            width="100%"
-            class="ma-auto">
-            <v-list-item-group>
-                <v-list-item
-                    v-for="(hero,index) in filterHeroes"
-                    :key="index"
-                    :to="{name: 'hero', params:{heroId: hero.name, heroData: hero}}">
-                        <v-list-item-content>
-                                <v-list-item-title 
-                                    v-text="hero.name"
-                                    class="text-center"/>
-                        </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
-        <p 
-            v-else 
-            class="text-center" 
-            v-text="findedHeroes"></p>
-    </v-container>
+  <v-container class="d-flex flex-column pa-0">
+    <div v-if="!!filteredHeroes(selected).length">
+      <div class="d-flex justify-center">
+        <v-select v-model="selected" :items="filter" class="flex-sm-grow-0" />
+      </div>
+      <v-list max-width="600" width="100%" class="ma-auto">
+        <v-list-item-group>
+          <v-list-item
+            v-for="(hero, index) in filteredHeroes(selected)"
+            :key="index"
+            :to="{ name: 'hero', params: { heroId: hero.name, heroData: hero } }"
+          >
+            <v-list-item-content>
+              <v-list-item-title class="text-center" v-text="hero.name" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </div>
+    <p v-else class="text-center">{{ error }}</p>
+  </v-container>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex';
 
 export default {
-    name: 'List',
-    data(){
-        return{
-            filter: ['All', 'Male', 'Female', 'Unknown'],
-            selected: 'All'
-        }
-    },
-    computed: {
-        findedHeroes(){
-            return this.$store.getters.returnHero
-        },
-        filterHeroes(){
-            return this.$store.getters.filteredHeroes(this.selected)
-        }
-    },
-}
+  name: 'List',
+  data() {
+    return {
+      filter: ['All', 'Male', 'Female', 'Unknown'],
+      selected: 'All',
+    };
+  },
+  computed: {
+    ...mapGetters(['filteredHeroes']),
+    ...mapState(['error']),
+  },
+};
 </script>
-
-<style>
-
-</style>
